d2bd643a1f3e6a2dc8273c704c2cfc3af18c5120
aikyoM3 <114334494+aikyoM3@users.noreply.github.com>
2025-03-23 23:01:19 +0700

    Version 1.0: Complete Game Logic

--- 2048/2048/2048.cpp
+++ 2048/2048/2048.cpp
@@ -1,62 +1,121 @@
-// 2048.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
-// Create a new blank board
 vector<vector<int>> createBoard() {
-    vector<vector<int>> board(4, vector<int>(4, 0)); // Initialize a 4x4 board with zeros (blank tiles)
-    return board;
+    return vector<vector<int>>(4, vector<int>(4, 0));
 }
 
-// Display the board
 void displayBoard(const vector<vector<int>>& board) {
-    int size = board.size();
-    for (int i = 0; i < size; ++i) {
-        cout << "+---+---+---+---+" << endl;
-        for (int j = 0; j < size; ++j) {
-            if (board[i][j] == 0) {
-                cout << "|   ";
-            }
-            else {
-                cout << "| " << board[i][j] << " ";
-            }
+    system("cls");  // Clear screen
+    cout << "\n2048 Game\n\n";
+    cout << "+------+------+------+------+\n";
+    for (const auto& row : board) {
+        for (int val : row) {
+            if (val == 0) cout << "|      ";
+            else cout << "| " << setw(4) << val << " ";
         }
-        cout << "|" << endl;
+        cout << "|\n+------+------+------+------+\n";
     }
-    cout << "+---+---+---+---+" << endl;
 }
 
-// Start a game loop
+void addRandomTile(vector<vector<int>>& board) {
+    vector<pair<int, int>> emptyTiles;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (board[i][j] == 0) emptyTiles.push_back({ i, j });
+
+    if (!emptyTiles.empty()) {
+        int idx = rand() % emptyTiles.size();
+        board[emptyTiles[idx].first][emptyTiles[idx].second] = (rand() % 10 < 9) ? 2 : 4;
+    }
+}
+
+vector<int> processLine(vector<int> line) {
+    vector<int> newLine(4, 0);
+    int index = 0;
+    for (int i = 0; i < 4; i++) {
+        if (line[i] == 0) continue;
+        if (index > 0 && newLine[index - 1] == line[i]) {
+            newLine[index - 1] *= 2;
+        }
+        else {
+            newLine[index++] = line[i];
+        }
+    }
+    return newLine;
+}
+
+void moveBoard(vector<vector<int>>& board, char move) {
+    vector<vector<int>> tempBoard = board;
+
+    if (move == 'W' || move == 'w') {
+        for (int col = 0; col < 4; col++) {
+            vector<int> line;
+            for (int row = 0; row < 4; row++) line.push_back(board[row][col]);
+            vector<int> newLine = processLine(line);
+            for (int row = 0; row < 4; row++) board[row][col] = newLine[row];
+        }
+    }
+    else if (move == 'S' || move == 's') {
+        for (int col = 0; col < 4; col++) {
+            vector<int> line;
+            for (int row = 3; row >= 0; row--) line.push_back(board[row][col]);
+            vector<int> newLine = processLine(line);
+            for (int row = 3; row >= 0; row--) board[row][col] = newLine[3 - row];
+        }
+    }
+    else if (move == 'A' || move == 'a') {
+        for (int row = 0; row < 4; row++) {
+            board[row] = processLine(board[row]);
+        }
+    }
+    else if (move == 'D' || move == 'd') {
+        for (int row = 0; row < 4; row++) {
+            vector<int> line(board[row].rbegin(), board[row].rend());
+            vector<int> newLine = processLine(line);
+            for (int col = 0; col < 4; col++) board[row][col] = newLine[3 - col];
+        }
+    }
+
+    if (board != tempBoard) addRandomTile(board);
+}
+
+bool isGameOver(const vector<vector<int>>& board) {
+    for (char move : {'W', 'A', 'S', 'D'}) {
+        vector<vector<int>> temp = board;
+        moveBoard(temp, move);
+        if (temp != board) return false;
+    }
+    return true;
+}
+
 void startGame() {
     vector<vector<int>> board = createBoard();
-    bool isOver = 0;
-/*    while (isOver != 1) {
+    addRandomTile(board);
+    addRandomTile(board);
+
+    while (true) {
         displayBoard(board);
+        if (isGameOver(board)) {
+            cout << "\nGame Over!\n";
+            break;
+        }
+        char move = _getch();
+        if (move == 27) break;
+        moveBoard(board, move);
     }
-*/
-    displayBoard(board);
 }
 
-
-
-
-
-
-
-int main()
-{
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    cout << "Welcome to 2048!\nUse W/A/S/D to move tiles. Press ESC to quit.\n";
     startGame();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
